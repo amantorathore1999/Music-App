57d84e33e8efbf3260a687a3e4c357691fe49753
amantorathore1999 <amantorathore1999@gmail.com>
2024-07-24 12:20:11 +0530

    Music App Upload
    sadasdasf

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// const MONGO_URI = "mongodb://127.0.0.1:27017/Spotify-clone";
+const MONGO_URI = "mongodb+srv://aman:aman@cluster0.crgpmwc.mongodb.net/"
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
